6450a7604a9d895567b402acec4719dcfda62244
Damien Saunders <damien@damien.co>
2012-07-22 16:00:25 +0100

    moved and renamed some files

---
+++ dbc_isotope/dbc-isotope-options.php
@@ -0,0 +1,62 @@
+<?php
+// Admin Panel
+?>
+
+<div class="wrap">
+	
+	<div id="icon-options-general" class="icon32"></div>
+	<h2>My Awesome Plugin</h2>
+	
+	<div class="metabox-holder has-right-sidebar">
+		
+<div class="inner-sidebar">
+
+			<div class="postbox">
+				<h3><span>Thanks from Damien</span></h3>
+				<div class="inside">
+		<p>Thanks for installing this. 
+		<br /><a target="_blank" href="http://damien.co/?utm_source=WordPress&utm_medium=dbc-sitewide-installed&utm_campaign=WordPress-Plugin">Damien</a></p> 
+		<p>Please add yourself to <a target="_blank" href="http://wordpress.damien.co/wordpress-mailing-list/?utm_source=WordPress&utm_medium=dbc-sitewide-installed&utm_campaign=WordPress-Plugin">my mailing list</a> to be the first to hear about updates for this plugin.</p>
+		<p>Let me and your friends know you installed this:</p>
+	<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://wordpress.damien.co/plugins" data-counturl="http://wordpress.damien.co/plugins" data-count="horizontal" data-via="damiensaunders" >Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>	
+
+				</div>
+			</div>
+
+			<div class="postbox">
+				<h3><span>Related Content</span></h3>
+				<div class="inside">
+					<p>Hi, I'm metabox 2!</p>
+					<ul>
+					<li><a href="http://wordpress.damien.co/dbc-sitewide/?utm_source=WordPress&utm_medium=dbc-sitewide-installed&utm_campaign=WordPress-Plugin">Help & FAQ's</a></li>
+					<li><a href="http://wordpress.damien.co/?utm_source=WordPress&utm_medium=dbc-sitewide-installed&utm_campaign=WordPress-Plugin">More WordPress Tips & Ideas</a></li>
+				</div>
+			</div>
+
+			<!-- ... more boxes ... -->
+
+		</div> <!-- .inner-sidebar -->
+
+		<div id="post-body">
+			<div id="post-body-content">
+
+				<div class="postbox">
+					<h3><span>Metabox 3</span></h3>
+					<div class="inside">
+						<p>Hi, I'm metabox 3!</p>
+					</div> <!-- .inside -->
+				</div>
+
+				<div class="postbox">
+					<h3><span>Metabox 4</span></h3>
+					<div class="inside">
+						<p>Hi, I'm metabox 4!</p>
+					</div> <!-- .inside -->
+				</div>
+
+			</div> <!-- #post-body-content -->
+		</div> <!-- #post-body -->
+
+	</div> <!-- .metabox-holder -->
+	
+</div> <!-- .wrap -->
--- dbc_isotope/dbc_isotope.php
+++ dbc_isotope/dbc_isotope.php
@@ -2,14 +2,14 @@
 /*
 Plugin Name: DBC Isotope
 Plugin URI: http://wordpress.damien.co/isotope?utm_source=WordPress&utm_medium=isotope&utm_campaign=WordPress-Plugin
-Description: A plugin to add Isotope & Masonry to your website. Caution needs commercial licence
-Version: 2.0
-Author: damiensaunders
+Description: A plugin to add Isotope & Masonry to your website. 
+Version: 0.1
+Author: Damien Saunders
 Author URI: http://damien.co/?utm_source=WordPress&utm_medium=isotope&utm_campaign=WordPress-Plugin
-License: GPLv2 or later
+License: Caution needs commercial licence
 */
 
-
+//hooks to add isotope jquery and custom stylesheet
 add_action('init', 'register_my_script');
 add_action('wp_footer', 'print_my_script');
 add_action( 'wp_print_styles', 'add_my_stylesheet' );
@@ -20,7 +20,7 @@ function register_my_script() {
 
 function add_my_stylesheet() {
         $myStyleUrl = plugins_url('style.css', __FILE__); 
-        $myStyleFile = WP_PLUGIN_DIR . '/jquery_isotope_damien/style.css';
+        $myStyleFile = WP_PLUGIN_DIR, __FILE__ . '/css/style.css';
         if ( file_exists($myStyleFile) ) {
             wp_register_style('myStyleSheets', $myStyleUrl);
             wp_enqueue_style( 'myStyleSheets');
@@ -31,5 +31,17 @@ function add_my_stylesheet() {
 function print_my_script() {
 	wp_print_scripts('isotope');
 }
+
+// Add Hook for Menu under Appearance
+add_action('admin_menu', 'dbc_isotope');
+function dbc_excludesitewide() 
+{
+	if(function_exists('add_menu_page')) 
+	{
+		add_theme_page('Isotope', 'Isotope', 'manage_options', dirname(__FILE__).'/dbc-isotope-options.php');
+	}
+}
+
+
 
 ?>
