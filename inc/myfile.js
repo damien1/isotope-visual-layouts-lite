be3c25d8a4ee655fd2a66ed2bc2d9c88fda0621f
Damien Saunders <damien@damien.co>
2013-02-18 23:07:40 +0000

    cleaned up jQuery and made it $afe

--- inc/myfile.js
+++ inc/myfile.js
@@ -1,9 +1,9 @@
 <!-- damien was here - if you change this please help me and MetaFizzy by buying a commercial licence -->					
 	<script type="text/javascript">
-	jQuery(document).ready(function() {
-    var mycontainer = jQuery('.isocontent');
-      mycontainer.isotope({
-      itemSelector: '.box'
-      });
+    jQuery(document).ready(function($){
+	    var container = $(".isocontent");
+        container.isotope({
+        itemSelector : '.box',
+        layoutMode : 'fitRows'
     });
    </script>
